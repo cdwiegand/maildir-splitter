4ad0095506ab1a4c3ad5bcc27e2dd79d87a87212
Chris Wiegand <chris@pinbn.com>
2021-05-22 00:08:12 -0600

    add args to process a single file
    Allow caller to specify a single file, or try to use `find` from OS if we want for 1m+ file directories..

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace maildir_splitter
@@ -9,62 +10,107 @@ namespace maildir_splitter
         static string theLock = "";
         static void Log(string s)
         {
-            lock(theLock)
+            lock (theLock)
             {
                 Console.WriteLine(s);
                 System.IO.File.AppendAllText("log.log", s + "\n");
             }
         }
+        static string GetArgIfPresent(string[] args, string prefixedOptionWithColon)
+        {
+            string found = null;
+            if (args != null && args.Length > 0)
+            {
+                string testVal = args.FirstOrDefault(p => p.StartsWith(prefixedOptionWithColon));
+                if (!string.IsNullOrEmpty(testVal)) found = testVal.Substring(prefixedOptionWithColon.Length);
+            }
+            return found;
+        }
+        static bool GetIfArgPresent(string[] args, string option)
+        {
+            return args != null && args.Length > 0 && args.Contains(option);
+        }
         static void Main(string[] args)
         {
             // maildir
-            string maildir = Environment.GetEnvironmentVariable("MAILDIR");
-            if (string.IsNullOrEmpty(maildir) && args.Length > 0) maildir = args[0];
+            string maildir = Environment.GetEnvironmentVariable("MAILDIR") ?? GetArgIfPresent(args, "-maildir:");
+
+            // folder(s) on which to operate
+            string folder = Environment.GetEnvironmentVariable("MAILFOLDERS") ?? GetArgIfPresent(args, "-folders:") ?? "cur,new"; // my defaults
+
+            string file = GetArgIfPresent(args, "-file:");
+
+            bool useFind = GetIfArgPresent(args, "-find");
+
+            // test config
             if (string.IsNullOrEmpty(maildir)) throw new Exception("No MAILDIR env variable found, no argument to program - aborting!");
             if (!System.IO.Directory.Exists(maildir)) throw new Exception("Maildir path invalid!");
             else Log($"Using Maildir: {maildir}");
 
-            // folder(s) on which to operate
-            string folder = Environment.GetEnvironmentVariable("MAILFOLDERS");
-            if (string.IsNullOrEmpty(folder) && args.Length > 1) folder = args[1];
-            if (string.IsNullOrEmpty(folder)) folder = "cur,new"; // my defaults
-            foreach (string folderName in folder.Split(','))
-                ProcessFolder(maildir, folderName.Trim());
+            // special case: give me a single file, and I'll process as if it's in a subdir of a MAILDIR (./cur/somethinghere.12345 -> ./2019/11/somethinghere.12345 or somethinghere.12345 -> ../2019/11/somethinghere.12345)
+            if (!string.IsNullOrEmpty(file))
+            {
+                if (System.IO.File.Exists(file))
+                    ProcessFile(maildir, EnsurePathFullyRooted(System.Environment.CurrentDirectory, file), newDir => EnsureDirectoryExists(newDir), 1, 1);
+                else
+                    throw new Exception("Invalid file specified: " + file);
+            }
+            else
+                foreach (string folderName in folder.Split(','))
+                    ProcessFolder(maildir, folderName.Trim(), useFind);
         }
-        static void ProcessFolder(string maildir, string folderName)
+        static void ProcessFolder(string maildir, string folderName, bool useFind)
         {
             string folderPath = System.IO.Path.Combine(maildir, folderName);
             if (!System.IO.Directory.Exists(folderPath)) throw new Exception($"Folder effective path {folderPath} invalid!");
             else Log($"Scanning folder {folderPath} for files...");
 
+            string[] files;
             //// cheat file of known files - useful if you have 1m+ files!
-            //string[] files = null;
-            //if (args.Length > 2)
-            //    files = System.IO.File.ReadAllLines(args[2]);
-            //else
-            string[] files = System.IO.Directory.GetFiles(folderPath);
+            if (useFind)
+            {
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    WorkingDirectory = System.IO.Path.Combine(maildir, folderName),
+                    FileName = "find",
+                    Arguments = "-type f",
+                    RedirectStandardOutput = true
+                };
+                Process p = Process.Start(psi);
+                while (!p.HasExited) { System.Threading.Thread.Sleep(1000); Console.Write("."); }
+                string output = p.StandardOutput.ReadToEnd();
+                files = output.Split('\n');
+            }
+            else
+                files = System.IO.Directory.GetFiles(folderPath);
 
-            // cache of subdirs we've created, so we can run quickly
-            List<string> knownSubDirs = new List<string>();
 
             for (int i = 0; i < files.Length; i++)
-                ProcessFile(maildir, folderPath, files[i], newDir =>
-                {
-                    if (!knownSubDirs.Contains(newDir))
-                    {
-                        System.IO.Directory.CreateDirectory(newDir);
-                        knownSubDirs.Add(newDir);
-                    }
-                }, i, files.Length);
+                ProcessFile(maildir, EnsurePathFullyRooted(folderPath, files[i]), newDir => EnsureDirectoryExists(newDir), i, files.Length);
         }
-        static void ProcessFile(string maildir, string folderPath, string filePath, Action<string> PreMove, int i, int filesLength)
+
+        static List<string> StaticCreatedDirectories = new List<string>();
+        static void EnsureDirectoryExists(string newDir)
+        {
+            // cache of subdirs we've created, so we can run quickly
+            if (!StaticCreatedDirectories.Contains(newDir))
+            {
+                System.IO.Directory.CreateDirectory(newDir);
+                StaticCreatedDirectories.Add(newDir);
+            }
+        }
+        static string EnsurePathFullyRooted(string folderPath, string file)
+        {
+            if (!System.IO.Path.IsPathRooted(file))
+                file = System.IO.Path.Combine(folderPath, file); // prepend working dir path to this "just file name"
+            return file;
+        }
+        static void ProcessFile(string maildir, string filePath, Action<string> PreMove, int i, int filesLength)
         {
             string logPrefix = $"{i + 1}/{filesLength}: ";
             try
             {
                 string fullPathFixed = filePath;
-                if (!System.IO.Path.IsPathRooted(fullPathFixed))
-                    fullPathFixed = System.IO.Path.Combine(folderPath, fullPathFixed); // prepend working dir path to this "just file name"
                 string[] contents = System.IO.File.ReadAllLines(fullPathFixed);
                 string dateStr = contents.FirstOrDefault(p => p.ToLower().StartsWith("date: "));
                 DateTime theDate = new DateTime(1970, 1, 1);
